deabfa6b566484dd19f59b182c20528baaf72b88
ALVINdimpos <alvindimpos@gmail.com>
2022-10-19 20:39:11 +0200

    networking

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,2 +1,4 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/\[from:(.*?)\] \[to:(.*?)\] \[flags:(.*?)\]/).join(",")
+arr=ARGV[0].scan(/(?<=from:|to:|flags:)(.+?(?=\]))/)
+pri=arr[0][0] + "," + arr[1][0] + "," + arr[2][0]
+puts pri
